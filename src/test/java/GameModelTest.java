b81100c2e817c24a9b313ba56df67f5b7311a156
Varga-Tóth Máté <iqkrf1@inf.elte.hu>
2024-04-03 23:39:55 +0200

    error in testCheckInteraction

--- src/test/java/GameModelTest.java
+++ src/test/java/GameModelTest.java
@@ -1,14 +1,27 @@
+import com.example.demo.BombermanApplication;
+import com.example.demo.Controller.InGameController;
 import com.example.demo.Model.GameModel;
+import javafx.application.Application;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameModelTest {
-    @Test
-    public void testAddition() {
-        // Arrange
-        GameModel gm = new GameModel("Jungle", null);
-        assertTrue(gm.checkInteraction(10,20,30,40) );
-
+    @BeforeAll
+    public static void initJavaFX() {
+        // Initialize JavaFX toolkit
+        //Application.launch(BombermanApplicationa.class);
     }
+    @Test
+    public void testCheckInteraction1() {
+        InGameController igc = new InGameController(null, "", "", "Dzsungel");
+        GameModel gm = new GameModel("Dzsungel", null);
+        assertTrue(gm.checkInteraction(10,20,30,40) );
+    }
+//    @Test
+//    public void testCheckInteraction2() {
+//        GameModel gm = new GameModel("Jungle", null);
+//        assertFalse(gm.checkInteraction(80,20,40,40) );
+//    }
 }
