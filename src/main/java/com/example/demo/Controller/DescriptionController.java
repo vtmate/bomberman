d172940930eabd94b3657d11126bc4e0db438024
Gönye Mihály <pznyy6@inf.elte.hu>
2024-04-03 13:14:17 +0200

    Update description UI

--- src/main/java/com/example/demo/Controller/DescriptionController.java
+++ src/main/java/com/example/demo/Controller/DescriptionController.java
@@ -1,5 +1,7 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.Player;
+import com.example.demo.Model.PowerUp;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -10,8 +12,9 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 
 import java.io.IOException;
@@ -21,8 +24,18 @@ import java.util.List;
 
 public class DescriptionController {
     GameController gc;
+    GridPane powerUpGrid;
+    private GridPane badPowerUpGrid;
+    private final int WIDTH = 700;
+    private int powerUpGridRows = 0;
+    private int badPowerUpGridRows = 0;
+    private GridPane controlGrid;
+    private int controlGridRows = 0;
+
     @FXML
     ScrollPane scrollPane;
+    @FXML
+    Label title;
     public DescriptionController(GameController gc) {
         this.gc = gc;
     }
@@ -30,9 +43,14 @@ public class DescriptionController {
     public void initialize() {
         scrollPane.setContent(new ImageView(new Image("bg.jpg")));
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setPrefWidth(600);
+        scrollPane.setPrefWidth(WIDTH);
+
+        Font adumuFont = Font.loadFont(getClass().getResourceAsStream("/Adumu.ttf"), 30);
+        title.setFont(adumuFont);
+
         VBox vbox = new VBox();
-        vbox.setPrefWidth(600);
+        vbox.setPrefWidth(WIDTH);
+        vbox.setStyle("-fx-padding: 10 20 10 10; -fx-background-color: #161C1C;");
 
         Label desc1 = new Label();
         desc1.setText("A játék egy 2 dimenziós pályán játszódik, amely négyzet alakú mezőkből áll. A játékot 2 játékos játsza, " +
@@ -42,53 +60,54 @@ public class DescriptionController {
                 "is). Egy játékos veszt (és ezáltal ellenfele győz), ha felrobban, vagy ha egy szörny elkapja.");
         desc1.setStyle("-fx-font-size: 20px;");
         desc1.setWrapText(true);
-        desc1.setMaxWidth(600);
+        desc1.setMaxWidth(WIDTH);
+        desc1.setTextFill(Color.WHITE);
         desc1.setTextAlignment(TextAlignment.JUSTIFY);
 
         Label controlTitle = new Label("Irányítás");
-        controlTitle.setStyle("-fx-font-size: 25px;");
+        controlTitle.setStyle("-fx-font-size: 25px; -fx-font-weight: 900; -fx-padding: 25 0 0 0");
+        controlTitle.setTextFill(Color.WHITE);
 
-        GridPane controlGrid = new GridPane(10,10);
-        controlGrid.setPrefWidth(600);
-        controlGrid.add(new Label("Esemény"), 1, 1);
-        controlGrid.add(new Label("1. játékos"), 2, 1);
-        controlGrid.add(new Label("2. játékos"), 3, 1);
+        controlGrid = new GridPane(0,0);
+        controlGrid.setPrefWidth(WIDTH);
 
-        controlGrid.add(new Label("Mozgás fel"), 1, 2);
-        controlGrid.add(new Label("W"), 2, 2);
-        controlGrid.add(new Label("UP"), 3, 2);
+        newControlDesc("Esemény", "1. játékos", "2. játékos");
+        newControlDesc("Mozgás fel", "W", "UP");
+        newControlDesc("Mozgás le", "S", "DOWN");
+        newControlDesc("Mozgás balra", "A", "LEFT");
+        newControlDesc("Mozgás jobbra", "D", "RIGHT");
+        newControlDesc("Bomba lehelyezése", "Q", "CTRL");
+        newControlDesc("Akadály lehelyezése", "E", "SPACE");
 
-        controlGrid.add(new Label("Mozgás le"), 1, 3);
-        controlGrid.add(new Label("S"), 2, 3);
-        controlGrid.add(new Label("DOWN"), 3, 3);
+        Label powerUpTitle = new Label("Bónuszok");
+        powerUpTitle.setStyle("-fx-font-size: 25px; -fx-font-weight: 900; -fx-padding: 25 0 0 0");
+        powerUpTitle.setTextFill(Color.WHITE);
 
-        controlGrid.add(new Label("Mozgás balra"), 1, 4);
-        controlGrid.add(new Label("A"), 2, 4);
-        controlGrid.add(new Label("LEFT"), 3, 4);
-
-        controlGrid.add(new Label("Mozgás jobbra"), 1, 5);
-        controlGrid.add(new Label("D"), 2, 5);
-        controlGrid.add(new Label("RIGHT"), 3, 5);
-
-        controlGrid.add(new Label("Bomba lehelyezése"), 1, 6);
-        controlGrid.add(new Label("Q"), 2, 6);
-        controlGrid.add(new Label("CTRL"), 3, 6);
-
-        controlGrid.add(new Label("Akadály lehelyezése"), 1, 7);
-        controlGrid.add(new Label("E"), 2, 7);
-        controlGrid.add(new Label("SPACE"), 3, 7);
-
-        Label powerUpTitle = new Label("Búnuszok");
-        powerUpTitle.setStyle("-fx-font-size: 25px;");
-
-        GridPane powerUpGrid = new GridPane(10,10);
-
-        powerUpGrid.add(new Label("Detonátor"), 1, 1);
-        powerUpGrid.add(new Label("a játékos által lehelyezett bomba / bombák ne időzítő hatására robbanjanak fel, hanem az utolsó bomba lehelyezése után,\n a bomba lehelyezés funkciót még egyszer használva, az összes bombája robbanjon fel a játékosnak."), 1, 2);
+        powerUpGrid = new GridPane(10,10);
+        powerUpGrid.setPrefWidth(WIDTH);
 
 
+        newPowerUpDesc("Bombák száma", "A játékos által lehelyezhető bombák száma 1-gyel növekszik.", "morebombsPowerUp.png");
+        newPowerUpDesc("Robbanás hatótáva", "A játékos bombáinak hatótávja mind a 4 irányba 1-1 mezővel növekszik.", "biggerRadiusP.png");
+        newPowerUpDesc("Detonátor",  " A játékos által lehelyezett bomba / bombák ne időzítő hatására robbanjanak fel, hanem az utolsó bomba lehelyezése után, a bomba lehelyezés funkciót még egyszer használva, az összes bombája robbanjon fel a játékosnak.", "detonator.png");
+        newPowerUpDesc("Görkorcsolya", "A játékos karakterének sebessége növekedjen meg. Nem halmozható, az esetleges második és további görkorcsolyák felvételének nincs hatása.", "rollerskateskaterPowerUp.png");
+        newPowerUpDesc("Sérthetetlenség ", "Egy rövid ideig a játékos karaktere legyen sérthetetlen. Vizuálisan is jelöljük, hogy a játékos jelenleg sérthetetlen, és azt is, ha hamarosan véget ér a hatása", "shieldPowerUp.png");
+        newPowerUpDesc("Szellem", "Egy rövid ideig a játékos karaktere legyen képes a falakon, a dobozokon és a bombákon is áthaladni. Vizuálisan is jelöljük, hogy a játékos rendelkezik a képességgel és azt is, ha hamarosan véget ér a hatása. Ha a játékos a képesség végekor egy doboz vagy egy fal mezőn tartózkodik, akkor meghal. (Bombáról leléphet.)", "ghostPowerUp.png");
+        newPowerUpDesc("Akadály", "A játékos helyezhessen le akadályokat, amelyek játéklogikailag úgy viselkednek, mint a dobozok, azonban ezekből bónusz (power-up) sosem kerül elő. Maximálisan 3 akadály legyen egy játékos által lehelyezhető. (Ha felrobbantásra kerülnek, lehelyezhető új.) Ez a képesség legyen halmozható, azaz a bónusz esetleges második felvételével 6 akadály legyen lehelyezhető, stb.", "gatePowerUp.png");
 
-        vbox.getChildren().addAll(desc1, controlTitle, controlGrid, powerUpTitle, powerUpGrid);
+        Label badPowerUpTitle = new Label("Hátráltató bónuszok");
+        badPowerUpTitle.setStyle("-fx-font-size: 25px; -fx-font-weight: 900; -fx-padding: 25 0 0 0");
+        badPowerUpTitle.setTextFill(Color.WHITE);
+
+        badPowerUpGrid = new GridPane(10,10);
+        badPowerUpGrid.setPrefWidth(WIDTH);
+
+        newBadPowerUpDesc("Lassítás", "A játékos karakterének sebessége egy időre csökkenjen le.", "snailPowerUp.png");
+        newBadPowerUpDesc("Hatótávzsugorítás", "A játékos által lehelyezett bombák hatótávja mindössze 1 mező legyen egy adott ideig.", "smallerRadiusP.png");
+        newBadPowerUpDesc("Bombamegvonás", "A játékos ne tudjon bombákat lehelyezni egy ideig.", "noBombs.png");
+        newBadPowerUpDesc("Azonnali bomba lehelyezés", "A játékos karaktere azonnal helyezze le a bombákat, amint tudja (szabad mezőn áll és tud lehelyezni bombát). Ez a hátráltatás is csak egy adott ideig fejtse ki hatását.", "immadiate.png");
+
+        vbox.getChildren().addAll(desc1, controlTitle, controlGrid, powerUpTitle, powerUpGrid, badPowerUpTitle, badPowerUpGrid);
         vbox.setAlignment(Pos.CENTER);
         vbox.setSpacing(10);
 
@@ -100,5 +119,79 @@ public class DescriptionController {
     protected void goToMainPage() throws IOException {
         System.out.println("Főoldal");
         this.gc.changeScene("mainPage");
+    }
+
+    private void newControlDesc(String eventData, String player1Data, String player2Data) {
+        controlGridRows++;
+        Label event = new Label(eventData);
+        event.setTextFill(Color.WHITE);
+        event.setStyle("-fx-pref-width: 250; -fx-font-size: 20; -fx-padding: 10 10 10 20");
+        controlGrid.add(event, 1, controlGridRows);
+        Label player1 = new Label(player1Data);
+        player1.setTextFill(Color.WHITE);
+        player1.setStyle("-fx-pref-width: 200; -fx-font-size: 20; -fx-padding: 10 10 10 20");
+        controlGrid.add(player1, 2, controlGridRows);
+        Label player2 = new Label(player2Data);
+        player2.setTextFill(Color.WHITE);
+        player2.setStyle("-fx-pref-width: 200; -fx-font-size: 20; -fx-padding: 10 10 10 20");
+        controlGrid.add(player2, 3, controlGridRows);
+
+        if (controlGridRows == 1) {
+            event.setStyle("-fx-pref-width: 250; -fx-font-size: 22; -fx-font-weight: 900; -fx-background-color: #343939; -fx-padding: 10 10 10 20");
+            player1.setStyle("-fx-pref-width: 200; -fx-font-size: 22; -fx-font-weight: 900; -fx-background-color: #343939; -fx-padding: 10 10 10 20");
+            player2.setStyle("-fx-pref-width: 200; -fx-font-size: 22; -fx-font-weight: 900; -fx-background-color: #343939; -fx-padding: 10 10 10 20");
+        }
+
+        if (controlGridRows % 2 == 1 && controlGridRows != 1) {
+            event.setStyle("-fx-pref-width: 250; -fx-font-size: 20; -fx-background-color: #252B2B; -fx-padding: 10 10 10 20");
+            player1.setStyle("-fx-pref-width: 200; -fx-font-size: 20; -fx-background-color: #252B2B; -fx-padding: 10 10 10 20");
+            player2.setStyle("-fx-pref-width: 200; -fx-font-size: 20; -fx-background-color: #252B2B; -fx-padding: 10 10 10 20");
+        }
+
+    }
+    private void newPowerUpDesc(String nameData, String descData, String image) {
+        HBox hbox = new HBox();
+
+        Label title = new Label(nameData);
+        title.setStyle("-fx-font-size: 25; -fx-font-weight: 900");
+        title.setTextFill(Color.WHITE);
+        ImageView iw = new ImageView(new Image(image));
+        iw.setFitWidth(30);
+        iw.setFitHeight(30);
+        hbox.getChildren().addAll(iw, title);
+        hbox.setStyle("-fx-padding: 10 0 0 0");
+
+        powerUpGrid.add(hbox, 1, powerUpGridRows++);
+
+        Label desc = new Label(descData);
+        desc.setMaxWidth(WIDTH);
+        desc.setWrapText(true);
+        desc.setTextFill(Color.WHITE);
+        desc.setStyle("-fx-font-size: 22");
+
+        powerUpGrid.add(desc, 1, powerUpGridRows++);
+    }
+
+    private void newBadPowerUpDesc(String nameData, String descData, String image) {
+        HBox hbox = new HBox();
+
+        Label title = new Label(nameData);
+        title.setStyle("-fx-font-size: 25; -fx-font-weight: 900");
+        title.setTextFill(Color.WHITE);
+        ImageView iw = new ImageView(new Image(image));
+        iw.setFitWidth(30);
+        iw.setFitHeight(30);
+        hbox.getChildren().addAll(iw, title);
+        hbox.setStyle("-fx-padding: 10 0 0 0");
+
+        badPowerUpGrid.add(hbox, 1, badPowerUpGridRows++);
+
+        Label desc = new Label(descData);
+        desc.setMaxWidth(WIDTH);
+        desc.setWrapText(true);
+        desc.setTextFill(Color.WHITE);
+        desc.setStyle("-fx-font-size: 22");
+
+        badPowerUpGrid.add(desc, 1, badPowerUpGridRows++);
     }
 }
