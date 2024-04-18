c48732fc96c7099df748716c55bd1078a6cfcb00
Gönye Mihály <pznyy6@inf.elte.hu>
2024-04-18 22:22:31 +0200

    LayoutCreator commented and cleaned

--- src/main/java/com/example/demo/Model/LayoutCreator.java
+++ src/main/java/com/example/demo/Model/LayoutCreator.java
@@ -1,7 +1,6 @@
 package com.example.demo.Model;
 
 import com.example.demo.Controller.InGameController;
-
 import java.util.Random;
 
 public class LayoutCreator {
@@ -9,11 +8,17 @@ public class LayoutCreator {
     private final String map;
     public InGameController igc;
 
+    /**
+     * Létrehozzuk, és beállítjuk az egyes entitások modelljét.
+     *
+     * @param gm    a játék modelljének átadása
+     * @param map   a pálya típusa
+     * @param igc   az InGameController
+     */
     public LayoutCreator(GameModel gm, String map, InGameController igc){
         this.gm = gm;
         this.map = map;
         this.igc = igc;
-        System.out.println("map: " + map);
 
         createWalls();
         setUpPlayers();
@@ -22,26 +27,26 @@ public class LayoutCreator {
         createPowerUps();
     }
 
+    /**
+     * Létrehozza, és beállítja a bónuszokat, ügyelve arra, hogy doboz alatt legyen.
+     */
     public void createPowerUps(){
-        System.out.println("createpowerups");
         int counter = 0;
         int numOfPowerUps = 4;
         while(counter < numOfPowerUps){
             boolean exists = false;
             Random random = new Random();
 
-            //random doboz kiválasztása
             int index = random.nextInt(gm.boxes.size());
             double x = gm.boxes.get(index).x;
             double y = gm.boxes.get(index).y;
 
             for(PowerUp powerUp : gm.powerUps){
                 if(x == powerUp.x && y == powerUp.y){
-                    exists = true; //ha már az adott doboz alatt van powerUp
+                    exists = true;
                 }
             }
             if(!exists){
-                //random powerUp kiválasztása
                 int typeIndex = random.nextInt(PowerUpType.values().length);
                 PowerUpType type = PowerUpType.values()[typeIndex];
                 gm.powerUps.add(new PowerUp(x, y, type, igc));
@@ -50,6 +55,9 @@ public class LayoutCreator {
         }
     }
 
+    /**
+     * Létrehozza, és beállítja a dobozokat a pálya típusának megfelelően.
+     */
     private void createBoxes(){
         switch(map){
             case "Dzsungel":
@@ -73,8 +81,10 @@ public class LayoutCreator {
         }
     }
 
+    /**
+     * Létrehozza, és beállítja a falakat a pálya típusának megfelelően.
+     */
     private void createWalls() {
-
         for (int i = 0; i < 13; i++) { //felső és alsó sor
             gm.edgeWalls.add(new EdgeWall(i*40, 0));
             gm.edgeWalls.add(new EdgeWall(i*40,40*10));
@@ -96,6 +106,9 @@ public class LayoutCreator {
         }
     }
 
+    /**
+     * Létrehozza, és beállítja a falakat a Dzsungel pályán.
+     */
     private void createDzsungel(){
         for (int i = 2; i < 11; i+=2) {
             for (int j = 2; j < 9; j+=2) {
@@ -103,6 +116,10 @@ public class LayoutCreator {
             }
         }
     }
+
+    /**
+     * Létrehozza, és beállítja a falakat a Pokol pályán.
+     */
     private void createPokol(){
         for (int i = 2; i < 11; i+=2) {
             gm.walls.add(new Wall(i*40, 2*40));
@@ -114,14 +131,20 @@ public class LayoutCreator {
             gm.walls.add(new Wall(i*40, 5*40));
         }
     }
+
+    /**
+     * Létrehozza, és beállítja a falakat a Vadnyugat pályán.
+     */
     private void createVadnyugat(){
         for (int i = 1; i < 9; i++) {
             gm.walls.add(new Wall(3*40, i*40));
             gm.walls.add(new Wall(9*40, (i+1) *40));
         }
-
     }
 
+    /**
+     * Létrehozza, és beállítja a két játékost.
+     */
     private void setUpPlayers() {
         Player player1 = new Player(40,40, 0, true);
         Player player2 = new Player(440,360, 1, false);
@@ -131,6 +154,10 @@ public class LayoutCreator {
         gm.players.add(player1);
         gm.players.add(player2);
     }
+
+    /**
+     * Létrehozza, és beállítja a szörnyeket.
+     */
     private void createMonsters() {
         Monster monster = new Monster(40*4, 40, true, 0);
         gm.monsters.add(monster);
