53359ea5f58119f8ea904a28fd2bceb12ec1be9c
Gönye Mihály <pznyy6@inf.elte.hu>
2024-04-18 22:23:45 +0200

    Gate commented and cleaned

--- src/main/java/com/example/demo/Model/Gate.java
+++ src/main/java/com/example/demo/Model/Gate.java
@@ -2,6 +2,14 @@ package com.example.demo.Model;
 
 public class Gate extends Entity{
     public Player owner;
+
+    /**
+     * Akadály létrehozása.
+     *
+     * @param x     az akadály x koordinátája
+     * @param y     az akadály y koordinátája
+     * @param owner az akadály tulajdonosa
+     */
     public Gate(double x, double y, Player owner) {
         super(x, y);
         this.owner = owner;
