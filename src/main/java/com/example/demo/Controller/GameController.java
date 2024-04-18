b6d66601996c205084caebb263bdda7a3bc49bd0
Gönye Mihály <pznyy6@inf.elte.hu>
2024-04-18 13:48:28 +0200

    GameController commented and cleaned

--- src/main/java/com/example/demo/Controller/GameController.java
+++ src/main/java/com/example/demo/Controller/GameController.java
@@ -1,12 +1,10 @@
 package com.example.demo.Controller;
 
 import com.example.demo.BombermanApplication;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class GameController {
@@ -16,11 +14,23 @@ public class GameController {
     protected String playerName1;
     protected String playerName2;
 
+    /**
+     * A program elindulásakor megjeleníti a főoldal kinézetét.
+     *
+     * @param stage         a program ablaka
+     * @throws IOException  ha a kinézet nem létezik
+     */
     public GameController(Stage stage) throws IOException {
         this.stage = stage;
         changeScene("mainPage");
     }
 
+    /**
+     * Megjeleníti a megfelelő kinézetet a paraméterből az ablakban.
+     *
+     * @param name          a kinézet neve
+     * @throws IOException  ha a kinézet nem létezik
+     */
     public void changeScene(String name) throws IOException {
         this.fxmlLoader = new FXMLLoader(BombermanApplication.class.getResource(name + "-view.fxml"));
 
@@ -30,7 +40,6 @@ public class GameController {
             case "description" -> this.fxmlLoader.setController(new DescriptionController(this));
             case "inGame" -> this.fxmlLoader.setController(new InGameController(this, playerName1, playerName2, map));
         }
-        System.out.println(map);
 
         Parent root = this.fxmlLoader.load();
         Scene scene = new Scene(root, 900, 600);
