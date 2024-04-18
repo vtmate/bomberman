5ca940f4ce0d8206fcd641e16fd6d8ea5fbec0e9
Varga-Tóth Máté <iqkrf1@inf.elte.hu>
2024-04-18 13:12:58 +0200

    Box commented and cleaned

--- src/main/java/com/example/demo/Model/Box.java
+++ src/main/java/com/example/demo/Model/Box.java
@@ -6,6 +6,16 @@ public class Box extends Entity{
     public Box(double x, double y) {
         super(x, y);
     }
+
+    /**
+     * Ha szellem a játékos, akkor rá tud menni a dobozokra,
+     * ezért meg kell nézni, hogy egy adott powerUp felett van-e
+     * doboz (,hogy így ne tudja felvenni)
+     * @param boxes     a játékban található aktív dobozok
+     * @param x         a powerUp x koordinátája
+     * @param y         a powerUp y koordinátája
+     * @return          hogy van-e a powerUpon aktív doboz
+     */
     public static boolean hasBoxOnTop(ArrayList<Box> boxes, double x, double y){
         for(Box box : boxes){
             if(box.x == x && box.y == y){
