3c06cda52f6ac35706e3c42a4556a675781b6571
Gönye Mihály <pznyy6@inf.elte.hu>
2024-04-18 21:41:43 +0200

    Wall commented and cleaned

--- src/main/java/com/example/demo/Model/Wall.java
+++ src/main/java/com/example/demo/Model/Wall.java
@@ -1,6 +1,11 @@
 package com.example.demo.Model;
 
 public class Wall extends Entity{
+    /**
+     *
+     * @param x a fal x koordinátája
+     * @param y a fal y koordinátája
+     */
     public Wall(double x, double y) {
         super(x, y);
     }
